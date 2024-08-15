40285bb5ca1a08d4f85724ba9415bf6b739a3c5b
Davi Mendes Ramos <davimendesramos123@gmail.com>
2024-08-14 22:03:48 -0300

    Tabbed Page

--- App AEE/Pages/HomePrincipalPage.xaml.cs
+++ App AEE/Pages/HomePrincipalPage.xaml.cs
@@ -1,6 +1,9 @@
+using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
+using TabbedPage = Microsoft.Maui.Controls.TabbedPage;
+
 namespace App_AEE.Pages;
 
-public partial class HomePrincipalPage : ContentPage
+public partial class HomePrincipalPage : TabbedPage
 {
 	public HomePrincipalPage()
 	{
